f24c37fc159d7bd48926944560a135b68bd5176f
Tom G Varik <tgvarik@me.com>
2014-12-02 00:27:27 -0800

    mathjax

--- Gruntfile.js
+++ Gruntfile.js
@@ -305,6 +305,14 @@ var _              = require('lodash'),
                     execOptions: {
                         env: 'NODE_ENV=' + process.env.NODE_ENV
                     }
+                },
+                ghostium: {
+                  command: 'grunt buildwithghost',
+                  options: {
+                    execOptions:{
+                      cwd: path.resolve(cwd + '/.build/release/content/themes/ghostium')
+                    }
+                  }
                 }
             },
 
@@ -491,7 +499,8 @@ var _              = require('lodash'),
                     }, {
                         expand: true,
                         src: buildGlob,
-                        dest: '<%= paths.releaseBuild %>/'
+                        dest: '<%= paths.releaseBuild %>/',
+                        dot: true
                     }]
                 }
             },
@@ -1062,6 +1071,18 @@ var _              = require('lodash'),
             ' - Clean out unnecessary files (travis, .git*, etc)\n' +
             ' - Zip files in release-folder to dist-folder/#{version} directory',
             ['init', 'concat:prod', 'copy:prod', 'emberBuildProd', 'uglify:release', 'clean:release', 'copy:release', 'compress:release']);
+
+        grunt.registerTask('deploy', [
+          'shell:bower',
+          'update_submodules',
+          'concat:prod',
+          'copy:prod',
+          'emberBuildProd',
+          'uglify:release',
+          'clean:release',
+          'copy:release',
+          'shell:ghostium'
+          ]);
     };
 
 // Export the configuration
